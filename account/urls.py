502c4526221ab925cac08d84005749f19086632d
hamburgduo <kanbaoduo@163.com>
2018-09-29 20:39:45 +0800

    增加密码重置.

--- account/urls.py
+++ account/urls.py
@@ -21,4 +21,12 @@ urlpatterns = [
         name='password_change'),
     url(r'^password-change-done/$', auth_views.password_change_done,
         {'template_name': 'account/password_change_done.html'}, name='password_change_done'),
+    url(r'^password-reset/$', auth_views.password_reset,
+        {'template_name': 'account/password_reset_form.html',
+         'email_template_name': 'account/password_reset_email.html',
+         'subject_template_name': 'account/password_reset_subject.txt',
+         'post_reset_redirect': '/account/password-reset-done'}, name='password_reset'),
+    url(r'^password-reset-done/$', auth_views.password_change_done,
+        {'template_name': 'account/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^password-reset-confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm)
 ]
